93203e04f00d7ea60bc0ced0059a9a849bf58e07
Luhan <575000607@qq.com>
2025-06-15 11:20:48 +0800

    20250615

--- src/main/java/edu/luhan/jun25/Demo450/tryCatchPracticeAnwser.java
+++ src/main/java/edu/luhan/jun25/Demo450/tryCatchPracticeAnwser.java
@@ -1,4 +1,26 @@
 package edu.luhan.jun25.Demo450;
 
+import java.util.Scanner;
+
 public class tryCatchPracticeAnwser {
+
+    public static void main(String[] args) {
+        // create the scanner
+        // use the loop and break to control
+
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        String input="";
+        while(true){
+            try {
+                input = sc.next();
+                num = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("please input a integer");;
+            }
+        }
+
+        System.out.println("your input is" + input);
+    }
 }
